b6e03d8a38fb8997aa7527dc1ff24734471c2720
JD01101998 <jahanvidave78247@gmail.com>
2021-10-28 20:59:30 -0500

    Feature CreateProfile Revised

--- lib/util/auth.dart
+++ lib/util/auth.dart
@@ -7,30 +7,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
-String? uid;
+String? userID;
 String? userEmail;
 Future<User?> signInWithEmailPassword(
-    String email, String password, BuildContext context) async {
+    String userEmail, String userPassword, BuildContext context) async {
   await Firebase.initializeApp();
   User? user;
 
   try {
     UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
+      userEmail: userEmail,
+      userPassword: userPassword,
     );
     user = userCredential.user;
 
     if (user != null) {
-      uid = user.uid;
-      userEmail = user.email;
+      userID = user.userID;
+      userEmail = user.userEmail;
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setBool('auth', true);
-      //print(jsonEncode(user));
-      await prefs.setString('useremail', userEmail.toString());
-      await prefs.setString('uid', uid.toString());
-      print(prefs.getString('useremail'));
+      SharedPreferences sharedPreference = await SharedPreferences.getInstance();
+      await sharedPreference.setBool('auth', true);
+      await sharedPreference.setString('useremail', userEmail.toString());
+      await sharedPreference.setString('userID', userID.toString());
     }
   } on FirebaseAuthException catch (e) {
     if (e.code == 'user-not-found') {
@@ -45,22 +43,22 @@ Future<User?> signInWithEmailPassword(
   return user;
 }
 
-Future<User?> registerWithEmailPassword(String email, String password) async {
+Future<User?> registerWithEmailPassword(String userEmail, String userPassword) async {
   // Initialize Firebase
   await Firebase.initializeApp();
   User? user;
 
   try {
     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
+      userEmail: userEmail,
+      userPassword: userPassword,
     );
 
     user = userCredential.user;
 
     if (user != null) {
-      uid = user.uid;
-      userEmail = user.email;
+      userID = user.userID;
+      userEmail = user.userEmail;
     }
   } catch (e) {
     print(e);
--- lib/util/database.dart
+++ lib/util/database.dart
@@ -3,45 +3,42 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 Future<void> storeValues() async {
-  String uid = '', email = '';
-  SharedPreferences.getInstance().then((prefs) => {
+  String userID = '', userEmail = '';
+  SharedPreferences.getInstance().then((sharedPreference) => {
         firestore.collection('User Profile').add({
-          'UID': '${prefs.getString('uid')}',
-          'Email': '${prefs.getString('useremail')}'
-        }).then((value) => {print('lol')})
+          'userID': '${sharedPreference.getString('userID')}',
+          'userEmail': '${sharedPreference.getString('userEmail')}'
+        }).then((value) => {print('Stored Value in DB')})
       });
 }
 
-Future<void> registerUser(String name, String email, String phone) async {
-  //String uid='', email='', name = '', phone = '';
-  SharedPreferences.getInstance().then((prefs) => {
+Future<void> registerUser(String userName, String userEmail, String userPhone) async {
+  SharedPreferences.getInstance().then((sharedPreference) => {
         firestore.collection('User Profile').add({
-          'Email': '${email}',
-          'Name': '${name}',
-          'Phone': '${phone}'
+          'userEmail': '${userEmail}',
+          'userName': '${userName}',
+          'userPhone': '${userPhone}'
         }).then((value) => {print('User Registered in DB')})
       });
 }
 
 Future<void> addRide(
-    String start, String end, String time, String email) async {
-  //String start='', end='', name = '', time = '';
-  SharedPreferences.getInstance().then((prefs) => {
+    String startLocation, String endLocation, String timeOfDay, String userEmail) async {
+  SharedPreferences.getInstance().then((sharedPreference) => {
         firestore.collection('ride-table').add({
-          'start': '${start}',
-          'end': '${end}',
-          'time': '${time}',
-          'email': '${email}'
+          'startLocation': '${startLocation}',
+          'endLocation': '${endLocation}',
+          'timeOfDay': '${timeOfDay}',
+          'userEmail': '${userEmail}'
         }).then((value) => {print('Added Ride in DB')})
       });
 }
 
 Future<dynamic> getUserData(String email) async {
-  Future<Map> data;
   try {
     var snapshot = await FirebaseFirestore.instance
         .collection('User Profile')
-        .where('Email', isEqualTo: email)
+        .where('userEmail', isEqualTo: userEmail)
         .get();
 
     return snapshot.docs[0].data();
@@ -51,16 +48,12 @@ Future<dynamic> getUserData(String email) async {
 }
 
 Future<List?> getRideData() async {
-  Future<Map> data;
   List rideList = [];
-  Map rideList1 = {};
   try {
     var snapshot =
         await FirebaseFirestore.instance.collection('ride-table').get();
     for (var doc in snapshot.docs) {
       rideList.add(doc.data());
-      print("InLoop");
-      print(doc.data());
     }
     return rideList;
   } catch (e) {
