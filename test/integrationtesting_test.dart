40b16fcc59504e235d66457496ea224b33fc51b9
malvaezrsettings <malvaezr05@gmail.com>
2021-11-30 19:44:30 -0600

    ....

--- test/integrationtesting_test.dart
+++ test/integrationtesting_test.dart
@@ -6,4 +6,10 @@ import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  // Test sign up functionality
+  testWidgets('Once the sign up button is pressed we are taken to the sign up screen and can start signing up',
+        (WidgetTester tester) async{
+        await tester.pumpWidget(widget)
+        },);
+  // Test log in fuctionality
 }
